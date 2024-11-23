a5ced1f0f86ab11e0b4fdf62705de9c6c6bc1d85
Michael Greifeneder <michael.greifeneder@george-labs.com>
2024-11-23 15:40:01 +0100

    Add markdown renderer

--- composeApp/src/commonMain/kotlin/com/github/mikegr/loksek/App.kt
+++ composeApp/src/commonMain/kotlin/com/github/mikegr/loksek/App.kt
@@ -1,37 +1,31 @@
 package com.github.mikegr.loksek
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import com.mikepenz.markdown.compose.Markdown
+import com.mikepenz.markdown.m3.markdownColor
+import com.mikepenz.markdown.m3.markdownTypography
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import loksek.composeapp.generated.resources.Res
-import loksek.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
-        }
+        MainScreen()
     }
+}
+
+@Composable
+fun MainScreen() {
+    Markdown( content =
+        """
+            # Header
+            * Liste
+            * More           
+            #Hashtags
+            
+        """.trimIndent(),
+        colors = markdownColor(),
+        typography = markdownTypography()
+    )
 }
